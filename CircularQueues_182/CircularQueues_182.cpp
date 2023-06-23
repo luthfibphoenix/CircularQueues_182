cf890e06b480f9cc9d3875ff973ad86e0780f768
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-23 11:17:59 +0700

    25

--- CircularQueues_182/CircularQueues_182.cpp
+++ CircularQueues_182/CircularQueues_182.cpp
@@ -98,3 +98,40 @@ public:
 		}
 	}
 };
+
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. exit" << endl;
+			cout << "Enter your choice (1-4): " << endl;
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.instert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case'3': {
+				q.display();
+				break;
+			}
+			case'4': {
+				return 0;
+			}
+			}
+		}
+	}
+}
