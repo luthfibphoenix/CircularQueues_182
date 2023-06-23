61d20bb43dda3d2de39b111aaa8ee7296dbea512
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-23 10:59:57 +0700

    22

--- CircularQueues_182/CircularQueues_182.cpp
+++ CircularQueues_182/CircularQueues_182.cpp
@@ -62,5 +62,23 @@ public:
 
 	void display() {
 		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement is the queue are....\n";
+
+		//jika FRONT_position <- REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
 };
