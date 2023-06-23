9de146a811350b73d7c64a8996903256c1853837
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-23 09:32:47 +0700

    14

--- CircularQueues_182/CircularQueues_182.cpp
+++ CircularQueues_182/CircularQueues_182.cpp
@@ -32,6 +32,9 @@ public:
 			//jika REAR berada di posisi terakhir array, kembali ke awal array
 			if (REAR == max - 1)
 				REAR = 0;
-		
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
